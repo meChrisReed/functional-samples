7fdd78285ca2405c2ddb3bc066e36c121b3111d9
Chris Reed <creed2@its.jnj.com>
2016-07-05 16:02:04 -0400

    Comments on utils

--- app/api/compose.js
+++ app/api/compose.js
@@ -1,3 +1,10 @@
+// compose takes a series of functions.
+// returns a function that when executed runs the series in order passing the
+// results of the previous to the next. Eventually returning the product of
+// the last supplied function
+
+// verb: any function that takes at least one value and returns one value
+// noun: an argument of any type
 // (verbs, noun) -> verbs(verbs(noun))
 const compose = (...args) => args.reduceRight(
 	(x,fn) => fn(x)
--- app/api/curry.js
+++ app/api/curry.js
@@ -1,3 +1,8 @@
+// curry takes a function and returns either the execution of that function or
+//   the curry, awaiting more arguments
+
+// verb: any function that takes at least one value and returns one value
+// nouns: any arguments of any type
 // (verb, nouns) -> curry(verb, nouns) | verb(nouns)
 const curry = (fn, ...args) =>
 	args.length === fn.length ?
